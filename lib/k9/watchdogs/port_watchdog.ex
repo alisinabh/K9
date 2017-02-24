1f743f9d21d41cfb98a255b45c6b539c4df57b4c
alisinabh <alisina.bm@gmail.com>
2017-02-24 14:02:16 +0330

    port watchdog bugfix on binary hostname

--- lib/k9/watchdogs/port_watchdog.ex
+++ lib/k9/watchdogs/port_watchdog.ex
@@ -6,6 +6,20 @@ defmodule K9.Watchdog.PortWatchdog do
 
   import K9.Tools.DnsTools
 
+  ###
+  # Watchdog API
+  ###
+
+  @doc """
+  Whatchdog bark api which checks for port availability on a host.
+
+  ## Parameters
+    - arg1: a tuple in format of ``{host, port, timeout}`` host is stgirng or an erlang ip, port is an int in range of 0-65536. timeout is int milliseconds.
+
+  ## Example
+    iex> K9.Watchdog.PortWatchdog.bark {"google.com", 80, 10000}
+    :ok
+  """
   def bark({server, port, timeout}) do
     {:ok, ip} = get_server_ip server
 
@@ -17,12 +31,16 @@ defmodule K9.Watchdog.PortWatchdog do
 
   def bark({server, port}), do: bark({server, port, 10000})
 
+  ###
+  # Port Watching tools
+  ###
+
   defp get_server_ip(fqdn) when is_list(fqdn) do
     {:ok, ip, _} = K9.Tools.DnsTools.resolve fqdn
     {:ok, ip}
   end
 
-  defp get_server_ip(fqdn) when is_binary(fqdn), do: fqdn |> get_server_ip
+  defp get_server_ip(fqdn) when is_binary(fqdn), do: fqdn |> String.to_charlist |> get_server_ip
 
   defp get_server_ip(ip = {_, _, _, _}), do: ip
 end
